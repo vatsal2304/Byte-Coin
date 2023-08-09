676da743e203f4742b2fde43571d1224e2715b99
vatsal <116101020+vatsal2304@users.noreply.github.com>
2023-08-09 12:41:17 +0530

    Add files via upload

---
+++ ByteCoin-iOS13-master/ByteCoin/Model/coinData.swift
@@ -0,0 +1,12 @@
+//
+//  coinData.swift
+//  ByteCoin
+//
+//  Created by Funnmedia 2 on 11/07/23.
+//  Copyright © 2023 The App Brewery. All rights reserved.
+//
+
+import Foundation
+struct coinData:Decodable{
+    let rate: Double
+}
